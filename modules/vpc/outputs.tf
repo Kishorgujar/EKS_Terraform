152fe009ff5fc75849debe9645268adde67dbddc
Kishorgujar <91127600+Kishorgujar@users.noreply.github.com>
2024-10-23 23:32:40 +0530

    Create outputs.tf

---
+++ modules/vpc/outputs.tf
@@ -0,0 +1,32 @@
+output "private_sub1_ids" {
+  value = aws_subnet.private_sub1[*].id
+}
+
+output "private_sub2_ids" {
+  value = aws_subnet.private_sub2[*].id
+}
+
+output "private_sub3_ids" {
+  value = aws_subnet.private_sub3[*].id
+}
+
+output "private_sub4_ids" {
+  value = aws_subnet.private_sub4[*].id
+}
+
+output "public_pub_subnet1_ids" {
+  value = aws_subnet.public_pub_sub1[*].id
+}
+
+output "public_pub_subnet2_ids" {
+  value = aws_subnet.public_pub_sub2[*].id
+}
+
+output "vpc_id" {
+  value = aws_vpc.public_vpc.id
+}
+output "security_group_id" {
+  description = "The ID of the security group created for the EKS cluster."
+  value       = aws_security_group.eks_security_group.id
+}
+
