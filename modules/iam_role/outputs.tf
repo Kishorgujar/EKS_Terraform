53d208ba26e0d0cded07fd27b86baaba9ce9bf7d
Kishorgujar <91127600+Kishorgujar@users.noreply.github.com>
2024-10-24 22:34:03 +0530

    Update outputs.tf

--- modules/iam_role/outputs.tf
+++ modules/iam_role/outputs.tf
@@ -1,11 +1,14 @@
 output "eks_role_arn" {
-  value = aws_iam_role.eks_role.arn
+  value       = aws_iam_role.eks_role.arn
+  description = "The ARN of the IAM role for the EKS cluster"
 }
 
 output "node_role_arn" {
   value = aws_iam_role.node_role.arn
 }
 
-output "AWS_LoadBalancer_Controller_Policy_ID" {
+output "load_balancer_controller_policy_arn" {
   value = aws_iam_policy.AWS_LoadBalancer_Controller_Policy.arn
+  description = "The ARN of the Load Balancer Controller IAM Policy"
 }
+
